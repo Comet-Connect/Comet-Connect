d34c703053a10b34a7d03fa3eccc4dc612778ca9
Kevin Fan <kevin.fan.72601@gmail.com>
2023-04-20 01:33:10 -0500

    more refactors

--- comet_connect_app/lib/auth/forgot_password.dart
+++ comet_connect_app/lib/auth/forgot_password.dart
@@ -10,11 +10,12 @@ class ForgotPassword extends StatefulWidget {
 
 class _ForgotPasswordState extends State<ForgotPassword> {
   final _usernameOrEmailController = TextEditingController(); // User Username
+  final _verificationCodeController = TextEditingController();
   final _passwordNewController = TextEditingController(); // User New Password
   final _passwordCfController =
       TextEditingController(); // User Confirm New Password
 
-  bool _enteredUsernameOrPassword = false;
+  bool _userFound = false;
   bool _matchingEmailCode = false;
   bool _obscurePassword = true; // Show password feature
   String auth = "chatappauthkey231r4"; // Authentication Key
@@ -23,7 +24,7 @@ class _ForgotPasswordState extends State<ForgotPassword> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Reset Password'),
+          title: const Text('Forgot Password'),
           // Return to main login screen
           leading: IconButton(
             icon: const Icon(Icons.arrow_back),
@@ -52,114 +53,145 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                             child: TextField(
                               keyboardType: TextInputType.emailAddress,
                               controller: _usernameOrEmailController,
+                              enabled: !_userFound,
                               decoration: const InputDecoration(
                                 hintText: 'Enter your username or email',
                                 border: OutlineInputBorder(),
                               ),
                             ),
                           ),
+                          const SizedBox(
+                            width: 20.0,
+                          ),
                           ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(10, 60)),
                               // TODO:
-                              onPressed: () {},
+                              onPressed: () {
+                                setState(() {
+                                  _userFound = true;
+                                });
+                              },
                               child: const Text('Submit'))
                         ],
                       ),
+                      // Verification code field
                       const SizedBox(height: 20.0),
-                      const SizedBox(height: 16.0),
-                      // toggle hidden password
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                            onPressed: () {
-                              setState(() {
-                                _obscurePassword = !_obscurePassword;
-                              });
-                            },
-                            child: Text(_obscurePassword ? 'Show' : 'Hide'),
-                          ),
-                        ],
-                      ),
-                      // new password field
-                      const Text(
-                        'New Password',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 10.0),
-                      TextField(
-                        obscureText: _obscurePassword,
-                        controller: _passwordNewController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Enter your new password',
-                        ),
-                      ),
-                      const SizedBox(height: 16.0),
-                      // toggle hidden password
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                            onPressed: () {
-                              setState(() {
-                                _obscurePassword = !_obscurePassword;
-                              });
-                            },
-                            child: Text(_obscurePassword ? 'Show' : 'Hide'),
-                          ),
-                        ],
-                      ),
-                      // confirm password field
-                      const Text(
-                        'Confirm Password',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 10.0),
-                      TextField(
-                        obscureText: _obscurePassword,
-                        controller: _passwordCfController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Confirm your new password',
-                        ),
-                      ),
-                      const SizedBox(height: 16.0),
-                      // toggle hidden password
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                            onPressed: () {
-                              setState(() {
-                                _obscurePassword = !_obscurePassword;
-                              });
-                            },
-                            child: Text(_obscurePassword ? 'Show' : 'Hide'),
-                          ),
-                        ],
-                      ),
 
-                      //reset button
-                      const SizedBox(height: 20.0),
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            reset(
-                                context,
-                                _usernameOrEmailController.text,
-                                _passwordNewController.text,
-                                _passwordCfController.text);
-                          },
-                          child: const Text('Reset'),
+                      if (_userFound) ...[
+                        const Text(
+                          'Enter Verification Code',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
+                        const SizedBox(height: 10.0),
+                        Row(children: [
+                          Expanded(
+                              child: TextField(
+                            controller: _verificationCodeController,
+                            enabled: !_matchingEmailCode,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter your verification code',
+                            ),
+                          )),
+                          const SizedBox(width: 20.0),
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(10, 60)),
+                              //  TODO:
+                              onPressed: () {
+                                setState(() {
+                                  _matchingEmailCode = true;
+                                });
+                              },
+                              child: const Text('Submit'))
+                        ]),
+                        const SizedBox(height: 20.0),
+                      ],
+
+                      if (_matchingEmailCode) ...[
+                        // new password field
+                        Row(children: [
+                          const Text(
+                            'New Password',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const Spacer(),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _obscurePassword = !_obscurePassword;
+                              });
+                            },
+                            child: Text(_obscurePassword ? 'Show' : 'Hide'),
+                          ),
+                        ]),
+                        const SizedBox(height: 10.0),
+                        TextField(
+                          obscureText: _obscurePassword,
+                          controller: _passwordNewController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter your new password',
+                          ),
+                        ),
+                        const SizedBox(height: 16.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            const Text(
+                              'Confirm Password',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const Spacer(),
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  _obscurePassword = !_obscurePassword;
+                                });
+                              },
+                              child: Text(_obscurePassword ? 'Show' : 'Hide'),
+                            ),
+                          ],
+                          // confirm password field
+                        ),
+                        const SizedBox(height: 10.0),
+                        TextField(
+                          obscureText: _obscurePassword,
+                          controller: _passwordCfController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Confirm your new password',
+                          ),
+                        ),
+                        const SizedBox(height: 16.0),
+
+                        //reset button
+                        const SizedBox(height: 20.0),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 50.0,
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              reset(
+                                  context,
+                                  _usernameOrEmailController.text,
+                                  _passwordNewController.text,
+                                  _passwordCfController.text);
+                            },
+                            child: const Text('Reset'),
+                          ),
+                        ),
+                      ],
                     ]))));
   } //Widget
 
